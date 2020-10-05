a8122fc68190d9a186cd3d365e7ba16df66d739f
premsai96400 <56737402+premsai96400@users.noreply.github.com>
2020-10-05 11:43:52 +0530

    Update web scraping.py

--- web scraping.py
+++ web scraping.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from twilio.rest import Client
 import time
-account_sid = "AC750e575cdbd1d6d1e73746d04f6be5e5" # enter the ssid of twilio account
-auth_token = "ad557c0881a8b1b4a393417e93b1d18d"  #enter the auth token of your twilio account
+account_sid = ""
+auth_token = ""
 path='C:\\Users\\Hi\\AppData\\Local\\Programs\\Python\\chromedriver' # enter the location of your chrome web driver
 driver=webdriver.Chrome(path)
 driver.get('https://www.covid19india.org/')
@@ -16,4 +16,4 @@ sai=res[0].find_all('h1')
 a = sai[0].get_text()
 b = 'corona cases in india are '+ a
 client = Client(account_sid, auth_token)
-message = client.messages.create( from_='+12013501575',body =b,to ='+91 9676192575')
+message = client.messages.create( from_='',body =b,to ='')
